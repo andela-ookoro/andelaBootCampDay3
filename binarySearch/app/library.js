a8472d75f9a10a4bc79dbc380e24e04b9f6d83ec
celestine <okorocelestine@gmail.com>
2017-01-11 06:33:16 +0100

    Add the test script for the binary search lab test

---
+++ binarySearch/app/library.js
@@ -0,0 +1,85 @@
+/**
+ * A class to extend the Array class and implement the binary search algorithm
+ * @Author okorocelstine@gmail.com (Okoro Celestine)
+ */
+'use strict'
+
+var MyArray =function() { 
+
+  // function to return the array [1,2,3,...,20]
+  Array.prototype.toTwenty= function() {
+        var series =[],
+            lastTerm = 20,
+            currentNumber =1;
+        while (currentNumber <= lastTerm ) {
+          series.push(currentNumber);
+          currentNumber++;
+        }
+        return series
+
+    }
+    
+  // function to return the array [2,4,6,...,40]
+  Array.prototype.toForty= function() {
+        var series =[],
+            lastTerm = 40,
+            currentNumber =2;
+        while (currentNumber <= lastTerm ) {
+          series.push(currentNumber);
+          currentNumber += 2;
+        }
+        return series
+    }
+
+  // function to return the array [10,20,30,...,1000]
+  Array.prototype.toOneThousand= function() {
+        var series =[],
+            lastTerm = 1000,
+            currentNumber =10;
+        while (currentNumber <= lastTerm ) {
+          series.push(currentNumber);
+          currentNumber += 10;
+        }
+        return series
+    }
+
+    // Function to search for a number in an array using binary search
+    Array.prototype.search= function(searchNum) {
+          var count = 0,
+              length = this.length,
+              index = 0,
+              startIndex = 0,
+              stopIndex = length - 1,
+              middle = Math.floor((stopIndex + startIndex)/2);
+
+
+          while(this[middle] != searchNum && startIndex < stopIndex){
+
+              //adjust search area
+              if (searchNum < this[middle]){
+                  stopIndex = middle - 1;
+              } else if (searchNum > this[middle]){
+                  startIndex = middle + 1;
+              }
+
+              //recalculate middle
+              middle = Math.floor((stopIndex + startIndex)/2);
+              count++;
+          }
+
+          //check if the num exists in the array, if not return -1
+          index =  (this[middle] != searchNum) ? -1 : middle;
+
+          var result = {
+            count : count,
+            index : index,
+            length : length
+          };
+          return result;
+    }
+
+
+}();
+
+module.exports = MyArray;
+
